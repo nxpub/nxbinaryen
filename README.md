a7f6b2c2dd470fbc852beaf5ca8e72010bea3e74
NX Maintainer <github@nx.pub>
2022-12-23 19:51:50 -0700

    [Day 5] Polishes setup.py

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # nxbinaryen
 Python Bindings for Binaryen
+
+### TODOs:
+* Add enums w/ cached values
+* Review the layout (split regions)
+
+### Problems:
+* num-params aren't reduced (degradation)
+* wrong ptr wrapping (see TypeExpand, TypeBuilderBuildAndDispose)
+* cffi vs overflow in LiteralFloat32Bits, LiteralFloat64Bits (see tests)
--- setup.py
+++ setup.py
@@ -1,7 +1,25 @@
 from setuptools import setup
 
 setup(
+    name='nxbinaryen',
+    version='1.111.0',
+    url='https://github.com/nxpub/nxbinaryen',
+    description='Python Bindings for Binaryen',
+    license='MIT License',
+    license_files=('LICENSE',),
+    zip_safe=False,
+    include_package_data=True,
     setup_requires=['cffi>=1.13.2'],
     install_requires=['cffi>=1.13.2'],
     cffi_modules=['./scripts/build_ffi.py:ffi'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Software Development :: Libraries',
+    ]
 )
